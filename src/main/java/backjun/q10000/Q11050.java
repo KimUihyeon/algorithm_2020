4d0b672941b9ab1e091d4aae68afe82df725bf14
KimUihyeon <dkrnl1318@naver.com>
2020-06-19 09:57:53 +0900

    feat:baekjun algorithm 10039 평균점수 11050 이항계수1 11051 이항계수2

---
+++ src/main/java/backjun/q10000/Q11050.java
@@ -0,0 +1,36 @@
+package backjun.q10000;
+
+import java.util.Scanner;
+
+/**
+ * 백준 알고리즘 Q11050
+ *
+ * @author Kimuihyeon
+ * @since 2020.06.19
+ *
+ * @Q
+ * 자연수 N과 정수 K가 주어졌을 때 이항 계수 (N,K) 를 구하는 프로그램을 작성하시오.
+ *
+ * @input
+ * 첫째 줄에 N과 K가 주어진다. (1 ≤  ≤ 10, 0 ≤  ≤ )
+ *
+ * @output
+ * (N,K) 를 출력한다.
+ *
+ */
+public class Q11050 {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        System.out.println(c(N,K));
+
+    }
+
+    public static int c(int n,int k){
+        if(k == 0 || k == n){
+            return 1;
+        }
+        return c(n-1 , k-1) +c(n-1 , k);
+    }
+}
