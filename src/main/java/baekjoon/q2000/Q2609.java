75a698b81469923f1e56f8180acee8755e8e93ff
kuh for mac <dkrnl1318@naver.com>
2020-06-28 21:55:53 +0900

    feat:baekjoon 1987 2609 10808 11655 (못푼거있음) 1987 소수찾기 2609 최소공배수 최대공약수 (못품) 10808 알파뱃 갯수 11655 ROT13

---
+++ src/main/java/baekjoon/q2000/Q2609.java
@@ -0,0 +1,33 @@
+package baekjoon.q2000;
+
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * 백준 알고리즘 Q2609
+ *
+ * @author kuh
+ * @since 2020.06.28
+ *
+ * @Q
+ * 두 개의 자연수를 입력받아 최대 공약수와 최소 공배수를 출력하는 프로그램을 작성하시오.
+ *
+ * @input
+ * 첫째 줄에는 두 개의 자연수가 주어진다. 이 둘은 10,000이하의 자연수이며 사이에 한 칸의 공백이 주어진다.
+ *
+ * @output
+ * 첫째 줄에는 입력으로 주어진 두 수의 최대공약수를, 둘째 줄에는 입력으로 주어진 두 수의 최소 공배수를 출력한다.
+ */
+public class Q2609 {
+    public static void main(String[] arg) throws IOException {
+        BufferedReader in =  new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+        in.close();
+        out.flush();
+        out.close();
+    }
+}
+    
